793103c9a167de07ff3b9ca2a6ac82c03e2b2e4e
Paulo Bittencourt <paulo.medeiros@grupofasitec.com.br>
2023-12-04 14:19:12 -0300

    Add project files.

---
+++ WatchYourWallet.Domain/Entities/Expenses.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchYourWallet.Domain.Entities
+{
+    public sealed class Expenses
+    {
+        public int ExpensesId { get; private set; }
+        public string Name { get; private set; }
+        public decimal Value { get; private set; }
+        public DateTime InitialDate { get; private set; }
+        public DateTime LastDate { get; private set; }
+        public int UserId { get; private set; }
+        public User Users { get; private set; }
+
+
+    }
+}
---
+++ WatchYourWallet.Domain/Entities/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WatchYourWallet.Domain.Validations;
+
+namespace WatchYourWallet.Domain.Entities
+{
+    public sealed class User
+    {
+        public int UserId { get; private set; }
+        public string Name { get; private set; }
+        public decimal? Salary { get; private set; }
+
+        public User(int userId, string name, decimal salary)
+        {
+
+            UserId = userId;
+            Name = name;
+            Salary = salary;
+        }
+
+        public User(string name, decimal salary)
+        {
+            ValidateDomain(name, salary);
+
+            Name = name;
+            Salary = salary;
+        }
+
+
+        public ICollection<Expenses> Expense { get; set; }
+
+        private void ValidateDomain(string name, decimal salary)
+        {
+            //Validação do Nome
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Nome inválido, o atributo é obrigatório");
+            DomainExceptionValidation.When(name.Length < 3, "Nome inválido, minomo de 3 caracteres");
+            Name = name;
+
+            //Validação do Salário
+            DomainExceptionValidation.When(decimal.IsNegative(salary), "O valor do salário é invalido.");
+            DomainExceptionValidation.When(salary.Equals(null) , "O valor do salário é obrigatório.");
+            Salary = salary;
+        }
+    }
+}
